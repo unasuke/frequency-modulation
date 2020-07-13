4fd24c43f5c3b66c2bfb993876fe64fb85befc1f
Yusuke Nakamura <yusuke1994525@gmail.com>
2020-07-13 17:49:57 +0900

    Type definition

---
+++ interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface EpisodeData {
+ id: number,
+ date: string,
+ title: string,
+ url: string,
+ guests: Guest[]
+}
+
+interface Guest {
+ name: string,
+ twitter: string
+}
+
+// interface Episodes: Array<Episode>
