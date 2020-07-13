66d6fe95c54b0ded79fa2543b8c5b545b4b6a93b
Yusuke Nakamura <yusuke1994525@gmail.com>
2020-07-13 17:58:21 +0900

    Update next.config.js (remove unused function)

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
-
 module.exports = {
   exportPathMap: async function(
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
-    const episodes = yaml.safeLoad(
-      fs.readFileSync('episodes.yaml', 'utf8'), { schema: yaml.FAILSAFE_SCHEMA }
-    );
-    console.log('################ Yaml loaded  ###########################');
     return {
-      // '/': { page: '/index', query: { episodes: episodes } },
-      // '/about': { page: '/about' },
-      // '/readme.md': { page: '/readme' },
-      // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
     }
   },
 }
