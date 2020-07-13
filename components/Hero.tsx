05231adf06c86598b7d81aa73fc4c225268603e0
Yusuke Nakamura <yusuke1994525@gmail.com>
2020-07-13 17:54:56 +0900

    Create Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from './Hero.module.css'
+
+export default function Hero() {
+  return <>
+    <h1 className={styles.hero}>
+      <img src={'/logo.svg'} alt='unasuke.fm' className={styles.logo} />
+    </h1>
+    </>
+}
